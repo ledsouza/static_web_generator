0d3119201cf759e91a89d92857f72d0e1b59fb6e
ledsouza <leandro.souza.159@gmail.com>
2024-05-13 19:51:44 -0300

    add function to copy files from one dir to another

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
-from textnode import TextNode
-from htmlnode import LeafNode
+import os
+import shutil
 
 
 def main() -> None:
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    path = "static"
+    destination = "public"
+    copy_files(path, destination)
+
+def copy_files(path, destination):
+    if not os.path.exists(destination) and not os.path.isfile(path):
+        os.mkdir(destination)
+    if os.path.exists(path) and os.path.isfile(path):
+        shutil.copy(path, destination)
+        print(f"{path} copied into {destination}")
+    elif os.path.exists(path) and not os.path.isfile(path):
+        new_paths = os.listdir(path)
+        for new_path in new_paths:
+            copy_files(os.path.join(path, new_path), os.path.join(destination, new_path))
 
 if __name__ == "__main__":
     main()
